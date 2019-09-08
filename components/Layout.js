564f6322210aa22c0a38f93005d093ab584b2554
Leo Danielsson <leo.danielsson@comhem.com>
2019-09-08 21:46:53 +0200

    remove redirect thing

--- components/Layout.js
+++ components/Layout.js
@@ -15,17 +15,6 @@ const typography = new Typography({
 });
 
 export default ({ children }) => {
-  useEffect(() => {
-    if (window.netlifyIdentity) {
-      window.netlifyIdentity.on('init', user => {
-        if (!user) {
-          window.netlifyIdentity.on('login', () => {
-            document.location.href = '/static/admin/';
-          });
-        }
-      });
-    }
-  });
   return (
     <div>
       <Head>
