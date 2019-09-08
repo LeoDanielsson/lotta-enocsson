e27319da1cbdfbbacfb993b55d42c848fd5fb1bb
Leo Danielsson <leo.danielsson@comhem.com>
2019-09-08 21:23:43 +0200

    add netflify identity redirect

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Head from 'next/head';
 import Header from './Header';
 import { eggshell, red, grey } from '../colors';
@@ -13,46 +14,59 @@ const typography = new Typography({
   bodyFontFamily: ['Open Sans', 'sans-serif']
 });
 
-export default ({ children }) => (
-  <div>
-    <Head>
-      <title>Lotta Enocsson - Skulptör</title>
-      <meta lang='sv' />
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <meta charSet='utf-8' />
-      <link
-        href='https://fonts.googleapis.com/css?family=Merriweather|Open+Sans&display=swap'
-        rel='stylesheet'
-      />
-    </Head>
-    <Header />
-    <main>
-      <div className='container'>{children}</div>
-    </main>
-    <Footer />
-    <style jsx global>{`
-      ${typography.toString()}
-      h1 {
-        text-align: center;
-        color: ${red};
-      }
-      body {
-        background: ${grey};
-        color: black;
-      }
-      main {
-        background: ${eggshell};
-      }
+export default ({ children }) => {
+  useEffect(() => {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on('init', user => {
+        if (!user) {
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/static/admin/';
+          });
+        }
+      });
+    }
+  });
+  return (
+    <div>
+      <Head>
+        <title>Lotta Enocsson - Skulptör</title>
+        <meta lang='sv' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta charSet='utf-8' />
+        <link
+          href='https://fonts.googleapis.com/css?family=Merriweather|Open+Sans&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <Header />
+      <main>
+        <div className='container'>{children}</div>
+      </main>
+      <Footer />
+      <style jsx global>{`
+        ${typography.toString()}
+        h1 {
+          text-align: center;
+          color: ${red};
+        }
+        body {
+          background: ${grey};
+          color: black;
+        }
+        main {
+          background: ${eggshell};
+        }
 
-      .container {
-        max-width: 1200px;
-        padding: 32px 16px;
-        margin: 0 auto;
-        min-height: 100vh;
-      }
-      p {
-        max-width: 50em;
-      }
-    `}</style>
-  </div>
-);
+        .container {
+          max-width: 1200px;
+          padding: 32px 16px;
+          margin: 0 auto;
+          min-height: 100vh;
+        }
+        p {
+          max-width: 50em;
+        }
+      `}</style>
+    </div>
+  );
+};
