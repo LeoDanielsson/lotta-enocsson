7907ce6c73aae72e1e6c5cc2a22213fffb0ae07d
Leo Danielsson <leo.danielsson@comhem.com>
2019-09-08 21:38:27 +0200

    add /admin redirect

---
+++ pages/admin.js
@@ -0,0 +1,13 @@
+import useRouter from 'next/router';
+
+const Admin = () => <h1>Admin</h1>;
+
+Admin.getInitialProps = ({ res }) => {
+  res.writeHead(302, {
+    Location: '/static/admin/index.html'
+  });
+  res.end();
+  return {};
+};
+
+export default Admin;
